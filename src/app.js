67e0d8ce830bc0d55fcf30d345551e4699201d44
asmahan <loveallah734@yahoo.com>
2018-09-28 13:32:04 +0200

    welcome in react app

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ReactApp = (
+  <div>Hello from React App</div>
+)
+
+ReactDOM.render(ReactApp, document.getElementById('app'));
