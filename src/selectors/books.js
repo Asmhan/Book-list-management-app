f8830dda009e5e8b966012d6efa3126132de08c2
asmahan <loveallah734@yahoo.com>
2018-09-30 13:02:16 +0200

    sort books by name

--- src/selectors/books.js
+++ src/selectors/books.js
@@ -1,6 +1,8 @@
 // select Books
  export const selectBooksPerPage = (books, {pageNumber, itemsCountPerPage} ) => {
-  return books.slice(pageNumber, itemsCountPerPage);
+  return books.sort((a, b) => {
+      return a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1;
+  }).slice(pageNumber, itemsCountPerPage);
 };
 
 
